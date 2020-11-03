dd54c63077e02cc49e9595c36cdd3d57d92e433f
Janosch Maier <janosch@crashtest-security.com>
2020-11-03 17:21:04 +0100

    add waiting for scan to finish

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const axios = require('axios')
 const core = require('@actions/core');
-const github = require('@actions/github');
+
+const wait = function (milliseconds) {
+    return new Promise((resolve) => {
+      if (typeof milliseconds !== 'number') {
+        throw new Error('milliseconds not a number');
+      }
+      setTimeout(() => resolve("done!"), milliseconds)
+    });
+  };
 
 try {
 
     // Setup general variables
     const apiEndpoint = 'https://api.crashtest.cloud/webhook';
+    const status = 100; // 100 = Queued
 
     // Load Configuration
     const crashtestWebhook = core.getInput('crashtest-webhook');
@@ -16,19 +25,48 @@ try {
     axios
         .post(`${apiEndpoint}/${crashtestWebhook}`)
         .then(res => {
-            console.log(`scanId: ${res.data.scanID}`)
-            console.log(res)
+            const scanId = res.data.data.scanId
+            console.log(`scanId: ${res.data.data.scanId}`)
         })
         .catch(error => {
-            console.error(error)
+            core.setFailed(error.message);
+            return
         })
 
-    if (pullRreport == 'true') {
-        console.log("Waiting for the scan to finish in order to download the report.")
+    if (scanId != undefined) {
+        console.log(`Started Scan for Webhook ${crashtestWebhook}. Scan ID is ${scanId}.`)
     } else {
-        console.log("Skipping the download of the scan report.");
+        core.setFailed(`Could not start Scan for Webhook ${crashtestWebhook}.`);
+        return
     }
+
+    if (pullRreport == 'false') {
+        console.log("Skipping the download of the scan report.");
+        return
+    }
+
+    while (status <= 101) {
+        console.log(`Scan Status currently is ${status} (101 = Running)`);
+
+        // Only poll every minute
+        await wait(60000);
+
+        // Refresh status
+        axios
+        .get(`${apiEndpoint}/${crashtestWebhook}/scans/${scanId}/status`)
+        .then(res => {
+            const status = res.data.data.status.status_code
+            console.log(`status: ${res.data.data.status.status_code}`)
+        })
+        .catch(error => {
+            core.setFailed(error.message);
+            return
+        })
+    }
+
+    console.log(`Scan finished with status ${status}.`)
 
 } catch (error) {
     core.setFailed(error.message);
+    return
 }
